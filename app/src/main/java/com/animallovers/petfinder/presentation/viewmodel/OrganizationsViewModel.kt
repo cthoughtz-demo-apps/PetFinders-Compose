45cb8f4eafb6b0541e4d2d6c9d5a150cfa10b58f
James Edwards <cthoughtz@gmail.com>
2025-04-29 16:45:19 -0400

    Set up basic structure to show LazyColumn in Orgs Tab

--- app/src/main/java/com/animallovers/petfinder/presentation/viewmodel/OrganizationsViewModel.kt
+++ app/src/main/java/com/animallovers/petfinder/presentation/viewmodel/OrganizationsViewModel.kt
@@ -5,12 +5,14 @@ import androidx.lifecycle.viewModelScope
 import com.animallovers.petfinder.domain.model.organization.GetOrganizationsResponse
 import com.animallovers.petfinder.domain.usecase.organization.GetOrganizationsUseCase
 import com.animallovers.petfinder.presentation.util.PetFinderResult
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class OrganizationsViewModel @Inject constructor(
     private val getOrganizationsUseCase: GetOrganizationsUseCase
 ) : ViewModel() {
--- app/src/main/java/com/animallovers/petfinder/presentation/views/pages/OrgsPage.kt
+++ app/src/main/java/com/animallovers/petfinder/presentation/views/pages/OrgsPage.kt
@@ -1,11 +1,15 @@
 package com.animallovers.petfinder.presentation.views.pages
 
+import android.content.DialogInterface.OnShowListener
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -17,10 +21,49 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.animallovers.petfinder.R
+import com.animallovers.petfinder.domain.model.organization.GetOrganizationsResponse
+import com.animallovers.petfinder.presentation.util.PetFinderResult
+import com.animallovers.petfinder.presentation.viewmodel.OrganizationsViewModel
+import com.animallovers.petfinder.presentation.viewmodel.TokenViewModel
+
+
+private const val TAG = "OrgsPage"
+@Composable
+fun OrgsPage(
+    getOrganizationsViewModel: OrganizationsViewModel = hiltViewModel(),
+    tokenViewModel: TokenViewModel = hiltViewModel(),
+) {
+
+    val orgResult =
+        getOrganizationsViewModel.getOrganizationsResultFlow.collectAsStateWithLifecycle()
+
+    tokenViewModel.authToken?.let {
+        getOrganizationsViewModel.getOrganizations(it)
+        GetData(orgResult)
+    }
+}
 
 @Composable
-fun OrgsPage(modifier: Modifier = Modifier) {
+fun GetData(orgResult: State<PetFinderResult<GetOrganizationsResponse>>) {
+
+    when(val orgData = orgResult.value) {
+        is PetFinderResult.Failure -> {
+            Log.d(TAG, "GetData: ${orgData.errorMessage}")
+        }
+        is PetFinderResult.Loading -> CircularProgressIndicator()
+        is PetFinderResult.Success -> {
+            ShowList(orgData.data)
+        }
+        else -> Log.d(TAG, "GetData: State None")
+    }
+}
+
+
+@Composable
+fun ShowList(data: GetOrganizationsResponse) {
     Column(
         modifier = Modifier
             .fillMaxSize()
